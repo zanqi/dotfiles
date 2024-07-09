6ef2f6942bb63bf9ae081c95d562fbcffb102605
Zanqi <zanqi.liang@gmail.com>
2024-07-08 18:09:46 -0700

    bk

--- Mackup/.config/fish/config.fish
+++ Mackup/.config/fish/config.fish
@@ -16,3 +16,17 @@ alias df="df -h"
 
 zoxide init fish | source
 zoxide init --cmd cd fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
